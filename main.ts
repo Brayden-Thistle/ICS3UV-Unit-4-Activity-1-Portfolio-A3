a83da8dcd939142dd4cb71bef1d6146d740c8d41
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-12-12 20:11:09 +0000

    added a program to count every integer between the users selected range

--- main.ts
+++ main.ts
@@ -2,18 +2,21 @@
  * @author Brayden Thistle
  * @version 1.0.0
  * @date 2025-12-12
- * @fileoverview this program will
+ * @fileoverview this program will count every integer between your selected range
  */
 
+//values
 let StartValue: number;
 let EndValue: number;
 
+// prompts with if statement
 StartValue = Number(prompt("Enter your start value."));
 EndValue = Number (prompt("Enter your end value"));
 if (EndValue < StartValue ) {
   console.log("Sorry, your ending value has to be larger than your starting value, please enter another value.");
 }
 
+//loop
 for (
   let currentvalue: number = StartValue; 
   currentvalue < EndValue;
